34bf99e5dc9b4c711b311889e4f4e63a98eb99de
Stefan Dösinger <stefan@codeweavers.com>
2017-05-30 16:52:36 +0200

    Primitive integration of the wrapper DLLs into the build script.

--- build.sh
+++ build.sh
@@ -22,8 +22,14 @@ make install
 cd ../qemu
 WINEBUILD=$DESTDIR/build/install/bin/winebuild CC="$DESTDIR/build/install/bin/winegcc -lpthread -lpthread_nonshared -DWINE_NOWINSOCK" CXX="$DESTDIR/build/install/bin/wineg++ -lpthread -lpthread_nonshared -DWINE_NOWINSOCK" $SRCDIR/qemu/configure --disable-bzip2 --disable-libusb --disable-sdl --disable-snappy --disable-virtfs --disable-opengl --python=/usr/bin/python2.7 --disable-xen --disable-lzo --disable-qom-cast-debug --disable-vnc --disable-seccomp --disable-strip --disable-hax --disable-gnutls --disable-nettle --disable-replication --disable-tpm --disable-gtk --disable-gcrypt --disable-linux-aio --disable-system --without-pixman --disable-tools --disable-linux-user --enable-windows-user
 WINEBUILD=$DESTDIR/build/install/bin/winebuild make
+mkdir -p $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll
+mkdir -p $DESTDIR/build/qemu/x86_64-windows-user/qemu_host_dll
 
-# Build the wrapper DLLs
+# Build the wrapper DLLs. FIXME: automate this better.
+cd ../dlls/kernel32
+PATH=$DESTDIR/build/install/bin make
+ln -s kernel32.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll
+ln -s kernel32.dll.so $DESTDIR/build/qemu/x86_64-windows-user/qemu_host_dll
 
 # Cross-compile the Wine DLLs for x86 to quick-start higher level DLLs without having to wrap everything
 # Copy selected higher level DLLs into the guest DLL directory.
