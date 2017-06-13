6e230c0a0a8e970868c342ad68a38cfe99665d8b
André Hentschel <nerv@dawncrow.de>
2017-06-13 16:48:39 +0200

    Avoid the need to install wine-host

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ mkdir -p build/qemu
 cd build/wine-host
 $SRCDIR/wine/configure --prefix=$DESTDIR/build/install
 make -j 4
-make install
 
 # Cross-Compile Wine for the guest platform to copy higher level DLLs from. Disabled for now.
 # Unfortunately this won't work for msvcrt because our msvcrt imports symbols from Linux libc.
@@ -27,41 +26,38 @@ make install
 
 # Build qemu
 cd ../qemu
-LIBS=-lpthread_nonshared WINEBUILD=$DESTDIR/build/install/bin/winebuild CC="$DESTDIR/build/install/bin/winegcc -lpthread -lpthread_nonshared -DWINE_NOWINSOCK" CXX="$DESTDIR/build/install/bin/wineg++ -lpthread -lpthread_nonshared -DWINE_NOWINSOCK" $SRCDIR/qemu/configure --disable-bzip2 --disable-libusb --disable-sdl --disable-snappy --disable-virtfs --disable-opengl --python=/usr/bin/python2.7 --disable-xen --disable-lzo --disable-qom-cast-debug --disable-vnc --disable-seccomp --disable-strip --disable-hax --disable-gnutls --disable-nettle --disable-replication --disable-tpm --disable-gtk --disable-gcrypt --disable-linux-aio --disable-system --without-pixman --disable-tools --disable-linux-user --disable-guest-agent --enable-windows-user
-WINEBUILD=$DESTDIR/build/install/bin/winebuild make -j 4
+LIBS=-lpthread_nonshared WINEBUILD=$DESTDIR/build/wine-host/tools/winebuild/winebuild CC="$DESTDIR/build/wine-host/tools/winegcc/winegcc -I$DESTDIR/build/wine-host/include -I$DESTDIR/wine/include -lpthread -lpthread_nonshared -DWINE_NOWINSOCK" CXX="$DESTDIR/build/wine-host/tools/winegcc/wineg++ -I$DESTDIR/build/wine-host/include -I$DESTDIR/wine/include -lpthread -lpthread_nonshared -DWINE_NOWINSOCK" $SRCDIR/qemu/configure --disable-bzip2 --disable-libusb --disable-sdl --disable-snappy --disable-virtfs --disable-opengl --python=/usr/bin/python2.7 --disable-xen --disable-lzo --disable-qom-cast-debug --disable-vnc --disable-seccomp --disable-strip --disable-hax --disable-gnutls --disable-nettle --disable-replication --disable-tpm --disable-gtk --disable-gcrypt --disable-linux-aio --disable-system --without-pixman --disable-tools --disable-linux-user --disable-guest-agent --enable-windows-user
+WINEBUILD=$DESTDIR/build/wine-host/tools/winebuild/winebuild make -j 4
 mkdir -p $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll
 mkdir -p $DESTDIR/build/qemu/x86_64-windows-user/qemu_host_dll
 
 # Build the wrapper DLLs. FIXME: automate this better.
 cd ../../dlls/ntdll
-PATH=$PATH:$DESTDIR/build/install/bin make
+make -j4
 ln -sf $PWD/ntdll.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll
 ln -sf $PWD/qemu_ntdll.dll.so $DESTDIR/build/qemu/x86_64-windows-user/qemu_host_dll
 
 cd ../../dlls/kernel32
-PATH=$PATH:$DESTDIR/build/install/bin make
+make -j4
 ln -sf $PWD/kernel32.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll
 ln -sf $PWD/qemu_kernel32.dll.so $DESTDIR/build/qemu/x86_64-windows-user/qemu_host_dll
 
 cd ../../dlls/msvcrt
-PATH=$PATH:$DESTDIR/build/install/bin make
+make -j4
 ln -sf $PWD/msvcrt.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll
 ln -sf $PWD/qemu_msvcrt.dll.so $DESTDIR/build/qemu/x86_64-windows-user/qemu_host_dll
 
 cd ../../dlls/advapi32
-PATH=$PATH:$DESTDIR/build/install/bin make
+make -j4
 ln -sf $PWD/advapi32.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll
 ln -sf $PWD/qemu_advapi32.dll.so $DESTDIR/build/qemu/x86_64-windows-user/qemu_host_dll
 
 cd ../../dlls/comdlg32
-PATH=$PATH:$DESTDIR/build/install/bin make
+make -j4
 ln -sf $PWD/comdlg32.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll
 ln -sf $PWD/qemu_comdlg32.dll.so $DESTDIR/build/qemu/x86_64-windows-user/qemu_host_dll
 
 cd ../../dlls/gdi32
-PATH=$PATH:$DESTDIR/build/install/bin make
+make -j4
 ln -sf $PWD/gdi32.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll
 ln -sf $PWD/qemu_gdi32.dll.so $DESTDIR/build/qemu/x86_64-windows-user/qemu_host_dll
-
-#cd $DESTDIR/build
-#ln -s wine-host/dlls/msvcrt/msvcrt.dll qemu/x86_64-windows-user/qemu_guest_dll
