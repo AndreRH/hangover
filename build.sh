f61275c82aeeb88181a602d461fec546e741f1c6
Stefan Dösinger <stefan@codeweavers.com>
2017-06-18 13:20:39 +0200

    Enable wine and qemu build again.

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ mkdir -p $DESTDIR/build/qemu
 
 # Build the Host (e.g. arm64) wine
 cd $DESTDIR/build/wine-host
-#$SRCDIR/wine/configure --prefix=$DESTDIR/build/install
-#make -j 4
+$SRCDIR/wine/configure --prefix=$DESTDIR/build/install
+make -j 4
 
 # Cross-Compile Wine for the guest platform to copy higher level DLLs from. Disabled for now.
 # Unfortunately this won't work for msvcrt because our msvcrt imports symbols from Linux libc.
@@ -23,8 +23,8 @@ cd $DESTDIR/build/wine-host
 
 # Build qemu
 cd $DESTDIR/build/qemu
-#LIBS=-lpthread_nonshared CC="$DESTDIR/build/wine-host/tools/winegcc/winegcc -B$DESTDIR/build/wine-host/tools/winebuild -I$DESTDIR/build/wine-host/include -I$DESTDIR/wine/include -lpthread -lpthread_nonshared -DWINE_NOWINSOCK" CXX="$DESTDIR/build/wine-host/tools/winegcc/wineg++ -B$DESTDIR/build/wine-host/tools/winebuild -I$DESTDIR/build/wine-host/include -I$DESTDIR/wine/include -lpthread -lpthread_nonshared -DWINE_NOWINSOCK" $SRCDIR/qemu/configure --disable-bzip2 --disable-libusb --disable-sdl --disable-snappy --disable-virtfs --disable-opengl --python=/usr/bin/python2.7 --disable-xen --disable-lzo --disable-qom-cast-debug --disable-vnc --disable-seccomp --disable-strip --disable-hax --disable-gnutls --disable-nettle --disable-replication --disable-tpm --disable-gtk --disable-gcrypt --disable-linux-aio --disable-system --without-pixman --disable-tools --disable-linux-user --disable-guest-agent --enable-windows-user
-#make -j 4
+LIBS=-lpthread_nonshared CC="$DESTDIR/build/wine-host/tools/winegcc/winegcc -B$DESTDIR/build/wine-host/tools/winebuild -I$DESTDIR/build/wine-host/include -I$DESTDIR/wine/include -lpthread -lpthread_nonshared -DWINE_NOWINSOCK" CXX="$DESTDIR/build/wine-host/tools/winegcc/wineg++ -B$DESTDIR/build/wine-host/tools/winebuild -I$DESTDIR/build/wine-host/include -I$DESTDIR/wine/include -lpthread -lpthread_nonshared -DWINE_NOWINSOCK" $SRCDIR/qemu/configure --disable-bzip2 --disable-libusb --disable-sdl --disable-snappy --disable-virtfs --disable-opengl --python=/usr/bin/python2.7 --disable-xen --disable-lzo --disable-qom-cast-debug --disable-vnc --disable-seccomp --disable-strip --disable-hax --disable-gnutls --disable-nettle --disable-replication --disable-tpm --disable-gtk --disable-gcrypt --disable-linux-aio --disable-system --without-pixman --disable-tools --disable-linux-user --disable-guest-agent --enable-windows-user
+make -j 4
 mkdir -p $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll
 mkdir -p $DESTDIR/build/qemu/x86_64-windows-user/qemu_host_dll
 
