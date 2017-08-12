ab691e50c60e564da62d86bd2a56b57b86761042
Stefan Dösinger <stefan@codeweavers.com>
2017-08-12 15:49:58 +0200

    Store FPU state in guest context on exceptions.

--- testprogs/kernel32/except.c
+++ testprogs/kernel32/except.c
@@ -2,10 +2,18 @@
 #include <stdio.h>
 
 static int global = 0;
+static CONTEXT compare_ctx;
 
 static LONG WINAPI my_filter(PEXCEPTION_POINTERS epointers)
 {
     printf("in my filter, %%rcx=%p\n", (void *)epointers->ContextRecord->Rcx);
+
+    if (memcmp(&epointers->ContextRecord->FltSave, &compare_ctx.FltSave, sizeof(compare_ctx.FltSave)))
+        printf("Float state differs.\n");
+    else
+        printf("Float state is equal.\n");
+
+
     epointers->ContextRecord->Rcx = (DWORD64)&global;
     return EXCEPTION_CONTINUE_EXECUTION;
 }
@@ -14,6 +22,12 @@ int main()
 {
     int *x = NULL;
     printf("Hello exception test.\n");
+    volatile float f = 123.4;
+    volatile float g = 456.7;
+
+    printf("Printing a float %f and another %f, multiplied %f to put junk into fpregs.\n",
+            f, g, f * g);
+    RtlCaptureContext(&compare_ctx);
 
     SetUnhandledExceptionFilter(my_filter);
 
