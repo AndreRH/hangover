9331874b5a2e2aa76d1d313eaaedc805cb3bdd96
André Hentschel <nerv@dawncrow.de>
2017-10-21 21:33:22 +0200

    Update Android build script

--- build.android.sh
+++ build.android.sh
@@ -13,17 +13,19 @@ DESTDIR=`pwd`
 mkdir -p $DESTDIR/build.android
 mkdir -p $DESTDIR/build.android/wine-host
 mkdir -p $DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a
+mkdir -p $DESTDIR/build.android/wine-guest
+mkdir -p $DESTDIR/build.android/wine-guest32
 mkdir -p $DESTDIR/build.android/qemu
 mkdir -p $DESTDIR/build.android/glib
 mkdir -p $DESTDIR/build.android/libs
 
 # Build libs
 cd $DESTDIR/build.android/
-wget -c ftp://ftp-osl.osuosl.org/pub/libpng/src/libpng15/libpng-1.5.28.tar.gz
-tar -xzf libpng-1.5.28.tar.gz
+wget -c ftp://ftp-osl.osuosl.org/pub/libpng/src/libpng15/libpng-1.5.30.tar.gz
+tar -xzf libpng-1.5.30.tar.gz
 wget -c https://download.savannah.gnu.org/releases/freetype/freetype-2.8.tar.gz
 tar -xzf freetype-2.8.tar.gz
-cd $DESTDIR/build.android/libpng-1.5.28
+cd $DESTDIR/build.android/libpng-1.5.30
 ./configure --host=aarch64-linux-android --prefix=$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a CXX=aarch64-linux-android-clang++ CC=aarch64-linux-android-clang
 make -j4 ; make install
 cd $DESTDIR/build.android/freetype-2.8
@@ -37,15 +39,21 @@ make -j 4 ; make install
 
 # Build the Host (e.g. arm64) wine
 cd $DESTDIR/build.android/wine-host
-$SRCDIR/wine/configure --host=aarch64-linux-android --with-wine-tools=../../build/wine-host --prefix=$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/ --libdir=$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/lib CXX=aarch64-linux-android-clang++ CC=aarch64-linux-android-clang LIBPNG_CFLAGS="-I$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/include/libpng15" LIBPNG_LIBS="-L$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/lib -lpng15" FREETYPE_CFLAGS="-I$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/include/freetype2" FREETYPE_LIBS="-L$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/lib -lm -lz -lpng15 -lfreetype"
+$SRCDIR/wine/configure --host=aarch64-linux-android --with-wine-tools=../../build/wine-host --disable-tests --prefix=$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/ --libdir=$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/lib CXX=aarch64-linux-android-clang++ CC=aarch64-linux-android-clang LIBPNG_CFLAGS="-I$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/include/libpng15" LIBPNG_LIBS="-L$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/lib -lpng15" FREETYPE_CFLAGS="-I$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/include/freetype2" FREETYPE_LIBS="-L$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/lib -lm -lz -lpng15 -lfreetype"
 make -j 4 ; make install
 ln -sf ../../build/wine-host/tools tools
 
-#FIXME: Build wine guest and 32 bit wine guest
+# Cross-Compile Wine for the guest platform to copy higher level DLLs from.
+cd ../wine-guest
+$SRCDIR/wine/configure --host=x86_64-w64-mingw32 --with-wine-tools=../wine-host --without-freetype --disable-tests
+make -j 4
+cd ../wine-guest32
+$SRCDIR/wine/configure --host=i686-w64-mingw32 --with-wine-tools=../wine-host --without-freetype --disable-tests
+make -j 4
 
 # Build qemu
 cd $DESTDIR/build.android/qemu
-CC="$DESTDIR/build.android/wine-host/tools/winegcc/winegcc -D__ANDROID_API__=22 -I$NDK_SYSROOT/usr/include -L$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/lib --sysroot=$DESTDIR/build.android/wine-host -b aarch64-linux-android -B$DESTDIR/build.android/wine-host/tools/winebuild -I$DESTDIR/build.android/wine-host/include -I$DESTDIR/wine/include -DWINE_NOWINSOCK" CXX="$DESTDIR/build.android/wine-host/tools/winegcc/wineg++ -I$NDK_SYSROOT/usr/include -L$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/lib --sysroot=$DESTDIR/build.android/wine-host -b aarch64-linux-android -B$DESTDIR/build.android/wine-host/tools/winebuild -I$DESTDIR/build.android/wine-host/include -I$DESTDIR/wine/include  -DWINE_NOWINSOCK" $SRCDIR/qemu/configure --disable-bzip2 --disable-libusb --disable-sdl --disable-snappy --disable-virtfs --disable-opengl --python=/usr/bin/python2.7 --disable-xen --disable-lzo --disable-qom-cast-debug --disable-vnc --disable-seccomp --disable-strip --disable-hax --disable-gnutls --disable-nettle --disable-replication --disable-tpm --disable-gtk --disable-gcrypt --disable-linux-aio --disable-system --without-pixman --disable-tools --disable-linux-user --disable-guest-agent --enable-windows-user
+CC="$DESTDIR/build.android/wine-host/tools/winegcc/winegcc -D__ANDROID_API__=22 -I$NDK_SYSROOT/usr/include -L$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/lib --sysroot=$DESTDIR/build.android/wine-host -b aarch64-linux-android -B$DESTDIR/build.android/wine-host/tools/winebuild -I$DESTDIR/build.android/wine-host/include -I$DESTDIR/wine/include -DWINE_NOWINSOCK" CXX="$DESTDIR/build.android/wine-host/tools/winegcc/wineg++ -I$NDK_SYSROOT/usr/include -L$DESTDIR/build.android/wine-host/dlls/wineandroid.drv/assets/arm64-v8a/lib --sysroot=$DESTDIR/build.android/wine-host -b aarch64-linux-android -B$DESTDIR/build.android/wine-host/tools/winebuild -I$DESTDIR/build.android/wine-host/include -I$DESTDIR/wine/include  -DWINE_NOWINSOCK" $SRCDIR/qemu/configure --disable-bzip2 --disable-libusb --disable-sdl --disable-snappy --disable-virtfs --disable-opengl --python=/usr/bin/python2.7 --disable-xen --disable-lzo --disable-qom-cast-debug --disable-vnc --disable-seccomp --disable-strip --disable-hax --disable-gnutls --disable-nettle --disable-replication --disable-tpm --disable-gtk --disable-gcrypt --disable-linux-aio --disable-system --disable-tools --disable-linux-user --disable-guest-agent --enable-windows-user
 make -j 4
 mkdir -p $DESTDIR/build.android/qemu/x86_64-windows-user/qemu_guest_dll64
 mkdir -p $DESTDIR/build.android/qemu/x86_64-windows-user/qemu_host_dll64
@@ -60,8 +68,8 @@ declare -a dlls=("ntdll" "kernel32" "msvcrt" "advapi32" "comctl32" "comdlg32" "d
 
 for dll in "${dlls[@]}"
 do
-    mkdir -p $DESTDIR/build/dlls64/$dll
-    cd $DESTDIR/build/dlls64/$dll
+    mkdir -p $DESTDIR/build.android/dlls64/$dll
+    cd $DESTDIR/build.android/dlls64/$dll
     echo "GUEST_CC=x86_64-w64-mingw32" > Makefile
     echo "SRCDIR=../../../dlls/$dll" >> Makefile
     echo "DESTDIR?=../../.." >> Makefile
@@ -74,8 +82,8 @@ do
     ln -sf $PWD/$dll.dll $DESTDIR/build.android/qemu/x86_64-windows-user/qemu_guest_dll64
     ln -sf $PWD/qemu_$dll.dll.so $DESTDIR/build.android/qemu/x86_64-windows-user/qemu_host_dll64
 
-    mkdir -p $DESTDIR/build/dlls32/$dll
-    cd $DESTDIR/build/dlls32/$dll
+    mkdir -p $DESTDIR/build.android/dlls32/$dll
+    cd $DESTDIR/build.android/dlls32/$dll
     echo "GUEST_CC=i686-w64-mingw32" > Makefile
     echo "SRCDIR=../../../dlls/$dll" >> Makefile
     echo "DESTDIR?=../../.." >> Makefile
@@ -90,19 +98,19 @@ do
 done
 
 # Link Wine libraries.
-declare -a wine_dlls=("dbghelp" "ole32" "oleaut32" "propsys" "rpcrt4" "urlmon" "windowscodecs" "netapi32" "dnsapi"
-        "crypt32" "dwmapi" "uxtheme" "setupapi" "wintrust" "wtsapi32" "pdh" "avrt" "cryptnet" "imagehlp" "cryptui"
-        "sensapi" "riched20")
-ln -sf $DESTDIR/build/wine-guest/libs/wine/libwine.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll64
-ln -sf $DESTDIR/build/wine-guest32/libs/wine/libwine.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll32
+declare -a wine_dlls=("dbghelp" "ole32" "oleaut32" "propsys" "rpcrt4" "urlmon" "windowscodecs" "netapi32" "dnsapi" "msimg32"
+        "crypt32" "dwmapi" "uxtheme" "setupapi" "wintrust" "wtsapi32" "pdh" "avrt" "cryptnet" "imagehlp" "cryptui" "sensapi"
+        "riched20")
+ln -sf $DESTDIR/build.android/wine-guest/libs/wine/libwine.dll $DESTDIR/build.android/qemu/x86_64-windows-user/qemu_guest_dll64
+ln -sf $DESTDIR/build.android/wine-guest32/libs/wine/libwine.dll $DESTDIR/build.android/qemu/x86_64-windows-user/qemu_guest_dll32
 
 for dll in "${wine_dlls[@]}"
 do
-    ln -sf $DESTDIR/build/wine-guest/dlls/$dll/$dll.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll64
-    ln -sf $DESTDIR/build/wine-guest32/dlls/$dll/$dll.dll $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll32
+    ln -sf $DESTDIR/build.android/wine-guest/dlls/$dll/$dll.dll $DESTDIR/build.android/qemu/x86_64-windows-user/qemu_guest_dll64
+    ln -sf $DESTDIR/build.android/wine-guest/dlls/$dll/$dll.dll $DESTDIR/build.android/qemu/x86_64-windows-user/qemu_guest_dll32
 done
-ln -sf $DESTDIR/build/wine-guest/dlls/winspool.drv/winspool.drv $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll64
-ln -sf $DESTDIR/build/wine-guest32/dlls/winspool.drv/winspool.drv $DESTDIR/build/qemu/x86_64-windows-user/qemu_guest_dll32
+ln -sf $DESTDIR/build.android/wine-guest/dlls/winspool.drv/winspool.drv $DESTDIR/build.android/qemu/x86_64-windows-user/qemu_guest_dll64
+ln -sf $DESTDIR/build.android/wine-guest32/dlls/winspool.drv/winspool.drv $DESTDIR/build.android/qemu/x86_64-windows-user/qemu_guest_dll32
 
 set +e
 
