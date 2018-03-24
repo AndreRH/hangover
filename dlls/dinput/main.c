1d6a392459065ddd5d092fcfc7c24d0f67bbf0ad
Stefan Dösinger <stefan@codeweavers.com>
2018-03-24 16:55:27 +0100

    dinput: Copypaste the class factory from Wine.

--- dlls/dinput/main.c
+++ dlls/dinput/main.c
@@ -137,9 +137,88 @@ HRESULT WINAPI DllCanUnloadNow(void)
     return S_FALSE;
 }
 
+struct class_factory
+{
+    IClassFactory IClassFactory_iface;
+    LONG          ref;
+};
+
+static inline struct class_factory *impl_from_IClassFactory(IClassFactory *iface)
+{
+    return CONTAINING_RECORD(iface, struct class_factory, IClassFactory_iface);
+}
+
+static HRESULT WINAPI class_factory_QueryInterface(IClassFactory *iface, const IID *iid, void **obj)
+{
+    struct class_factory *fac = impl_from_IClassFactory(iface);
+
+    WINE_FIXME("(%p)->(%s,%p),stub!\n", fac, wine_dbgstr_guid(iid), obj);
+    return E_NOINTERFACE;
+}
+
+static ULONG WINAPI class_factory_AddRef(IClassFactory *iface)
+{
+    struct class_factory *fac = impl_from_IClassFactory(iface);
+    return InterlockedIncrement(&(fac->ref));
+}
+
+static ULONG WINAPI class_factory_Release(IClassFactory *iface)
+{
+    struct class_factory *fac = impl_from_IClassFactory(iface);
+    /* static class, won't be  freed */
+    return InterlockedDecrement(&(fac->ref));
+}
+
+static HRESULT WINAPI class_factory_CreateInstance(IClassFactory *iface, IUnknown *outer,
+        const IID *iid, void **obj)
+{
+    struct class_factory *fac = impl_from_IClassFactory(iface);
+
+    WINE_TRACE("(%p)->(%p,%s,%p)\n", fac, outer, wine_dbgstr_guid(iid), obj);
+    if (IsEqualGUID(&IID_IUnknown, iid) ||
+            IsEqualGUID(&IID_IDirectInputA, iid) ||
+            IsEqualGUID(&IID_IDirectInputW, iid) ||
+            IsEqualGUID(&IID_IDirectInput2A, iid) ||
+            IsEqualGUID(&IID_IDirectInput2W, iid) ||
+            IsEqualGUID(&IID_IDirectInput7A, iid) ||
+            IsEqualGUID(&IID_IDirectInput7W, iid))
+    {
+        return create_directinput_instance(iid, obj, NULL);
+    }
+
+    WINE_FIXME("(%p,%p,%s,%p) Interface not found!\n", fac, outer, wine_dbgstr_guid(iid), obj);	
+    return E_NOINTERFACE;
+}
+
+static HRESULT WINAPI class_factory_LockServer(IClassFactory *iface, BOOL dolock)
+{
+    struct class_factory *fac = impl_from_IClassFactory(iface);
+    WINE_FIXME("(%p)->(%d),stub!\n", fac, dolock);
+    return S_OK;
+}
+
+static const IClassFactoryVtbl class_factory_Vtbl =
+{
+    class_factory_QueryInterface,
+    class_factory_AddRef,
+    class_factory_Release,
+    class_factory_CreateInstance,
+    class_factory_LockServer
+};
+
+static struct class_factory class_factory = {{&class_factory_Vtbl}, 1};
+
 HRESULT WINAPI DllGetClassObject(const CLSID *rclsid, const IID *iid, void **obj)
 {
-    WINE_FIXME("(%s,%s,%p): Stub!\n", wine_dbgstr_guid(rclsid), wine_dbgstr_guid(iid), obj);
+    WINE_TRACE("(%s,%s,%p)\n", wine_dbgstr_guid(rclsid), wine_dbgstr_guid(iid), obj);
+    if (IsEqualCLSID( &IID_IClassFactory, iid))
+    {
+        *obj = &class_factory;
+        IClassFactory_AddRef((IClassFactory *)*obj);
+        return S_OK;
+    }
+
+    WINE_FIXME("(%s,%s,%p): no interface found.\n", wine_dbgstr_guid(rclsid), wine_dbgstr_guid(iid), obj);
     return CLASS_E_CLASSNOTAVAILABLE;
 }
 
