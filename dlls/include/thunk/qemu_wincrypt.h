d8597cbe76eaffab6dcfc5609a1c9c9ebf0c70d5
Stefan Dösinger <stefan@codeweavers.com>
2018-05-19 20:40:19 +0200

    Add the wincrypt thunk header.
    Should be in one of the earlier patches.

---
+++ dlls/include/thunk/qemu_wincrypt.h
@@ -0,0 +1,49 @@
+#ifndef HAVE_QEMU_THUNK_WINCRYPT_H
+#define HAVE_QEMU_THUNK_WINCRYPT_H
+
+struct qemu_CERT_CONTEXT
+{
+    DWORD      dwCertEncodingType;
+    qemu_ptr   pbCertEncoded; /* BYTE * */
+    DWORD      cbCertEncoded;
+    qemu_ptr   pCertInfo; /* CERT_INFO * */
+    qemu_ptr   hCertStore; /* HCERTSTORE */
+};
+
+static inline void CERT_CONTEXT_g2h(CERT_CONTEXT *host, const struct qemu_CERT_CONTEXT *guest)
+{
+    host->dwCertEncodingType = guest->dwCertEncodingType;
+    host->pbCertEncoded = (BYTE *)(ULONG_PTR)guest->pbCertEncoded;
+    host->cbCertEncoded = guest->cbCertEncoded;
+    host->pCertInfo = (CERT_INFO *)(ULONG_PTR)guest->pCertInfo;
+    host->hCertStore = (HCERTSTORE)(ULONG_PTR)guest->hCertStore;
+}
+
+static inline void CERT_CONTEXT_h2g(struct qemu_CERT_CONTEXT *guest, const CERT_CONTEXT *host)
+{
+    guest->dwCertEncodingType = host->dwCertEncodingType;
+    guest->pbCertEncoded = (ULONG_PTR)host->pbCertEncoded;
+    guest->cbCertEncoded = host->cbCertEncoded;
+    guest->pCertInfo = (ULONG_PTR)host->pCertInfo;
+    guest->hCertStore = (ULONG_PTR)host->hCertStore;
+}
+
+struct qemu_CRYPT_DATA_BLOB
+{
+    DWORD       cbData;
+    qemu_ptr    pbData;
+};
+
+static inline void CRYPT_DATA_BLOB_g2h(CRYPT_DATA_BLOB *host, const struct qemu_CRYPT_DATA_BLOB *guest)
+{
+    host->cbData = guest->cbData;
+    host->pbData = (BYTE *)(ULONG_PTR)guest->pbData;
+}
+
+static inline void CRYPT_DATA_BLOB_h2g(struct qemu_CRYPT_DATA_BLOB *guest, const CRYPT_DATA_BLOB *host)
+{
+    guest->cbData = host->cbData;
+    guest->pbData = (ULONG_PTR)host->pbData;
+}
+
+#endif
