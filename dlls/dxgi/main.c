abec2ee620fd75f3f8ed4078876c4d7798e0d873
Stefan Dösinger <stefan@codeweavers.com>
2018-05-21 19:33:28 +0200

    dxgi: Generate dxgi_main.c.

--- dlls/dxgi/main.c
+++ dlls/dxgi/main.c
@@ -21,6 +21,8 @@
 
 #include <windows.h>
 #include <stdio.h>
+#include <d3d10_1.h>
+#include <dxgi1_2.h>
 
 #include "thunk/qemu_windows.h"
 
@@ -28,9 +30,190 @@
 #include "dll_list.h"
 #include "qemu_dxgi.h"
 
-#ifndef QEMU_DLL_GUEST
+#ifdef QEMU_DLL_GUEST
+#include <debug.h>
+#else
 #include <wine/debug.h>
+#endif
+
 WINE_DEFAULT_DEBUG_CHANNEL(qemu_dxgi);
+
+struct qemu_CreateDXGIFactory2
+{
+    struct qemu_syscall super;
+    uint64_t flags;
+    uint64_t iid;
+    uint64_t factory;
+};
+
+#ifdef QEMU_DLL_GUEST
+
+WINBASEAPI HRESULT WINAPI CreateDXGIFactory2(UINT flags, REFIID iid, void **factory)
+{
+    struct qemu_CreateDXGIFactory2 call;
+    call.super.id = QEMU_SYSCALL_ID(CALL_CREATEDXGIFACTORY2);
+    call.flags = flags;
+    call.iid = (ULONG_PTR)iid;
+    call.factory = (ULONG_PTR)factory;
+
+    qemu_syscall(&call.super);
+
+    return call.super.iret;
+}
+
+#else
+
+extern HRESULT WINAPI CreateDXGIFactory2(UINT flags, REFIID iid, void **factory);
+void qemu_CreateDXGIFactory2(struct qemu_syscall *call)
+{
+    struct qemu_CreateDXGIFactory2 *c = (struct qemu_CreateDXGIFactory2 *)call;
+    WINE_FIXME("Unverified!\n");
+    c->super.iret = CreateDXGIFactory2(c->flags, QEMU_G2H(c->iid), QEMU_G2H(c->factory));
+}
+
+#endif
+
+struct qemu_CreateDXGIFactory1
+{
+    struct qemu_syscall super;
+    uint64_t iid;
+    uint64_t factory;
+};
+
+#ifdef QEMU_DLL_GUEST
+
+WINBASEAPI HRESULT WINAPI CreateDXGIFactory1(REFIID iid, void **factory)
+{
+    struct qemu_CreateDXGIFactory1 call;
+    call.super.id = QEMU_SYSCALL_ID(CALL_CREATEDXGIFACTORY1);
+    call.iid = (ULONG_PTR)iid;
+    call.factory = (ULONG_PTR)factory;
+
+    qemu_syscall(&call.super);
+
+    return call.super.iret;
+}
+
+#else
+
+void qemu_CreateDXGIFactory1(struct qemu_syscall *call)
+{
+    struct qemu_CreateDXGIFactory1 *c = (struct qemu_CreateDXGIFactory1 *)call;
+    WINE_FIXME("Unverified!\n");
+    c->super.iret = CreateDXGIFactory1(QEMU_G2H(c->iid), QEMU_G2H(c->factory));
+}
+
+#endif
+
+struct qemu_CreateDXGIFactory
+{
+    struct qemu_syscall super;
+    uint64_t iid;
+    uint64_t factory;
+};
+
+#ifdef QEMU_DLL_GUEST
+
+WINBASEAPI HRESULT WINAPI CreateDXGIFactory(REFIID iid, void **factory)
+{
+    struct qemu_CreateDXGIFactory call;
+    call.super.id = QEMU_SYSCALL_ID(CALL_CREATEDXGIFACTORY);
+    call.iid = (ULONG_PTR)iid;
+    call.factory = (ULONG_PTR)factory;
+
+    qemu_syscall(&call.super);
+
+    return call.super.iret;
+}
+
+#else
+
+void qemu_CreateDXGIFactory(struct qemu_syscall *call)
+{
+    struct qemu_CreateDXGIFactory *c = (struct qemu_CreateDXGIFactory *)call;
+    WINE_FIXME("Unverified!\n");
+    c->super.iret = CreateDXGIFactory(QEMU_G2H(c->iid), QEMU_G2H(c->factory));
+}
+
+#endif
+
+struct qemu_DXGID3D10CreateDevice
+{
+    struct qemu_syscall super;
+    uint64_t d3d10core;
+    uint64_t factory;
+    uint64_t adapter;
+    uint64_t flags;
+    uint64_t feature_levels;
+    uint64_t level_count;
+    uint64_t device;
+};
+
+#ifdef QEMU_DLL_GUEST
+
+WINBASEAPI HRESULT WINAPI DXGID3D10CreateDevice(HMODULE d3d10core, IDXGIFactory *factory, IDXGIAdapter *adapter,
+        unsigned int flags, const D3D_FEATURE_LEVEL *feature_levels, unsigned int level_count, void **device)
+{
+    struct qemu_DXGID3D10CreateDevice call;
+    call.super.id = QEMU_SYSCALL_ID(CALL_DXGID3D10CREATEDEVICE);
+    call.d3d10core = (ULONG_PTR)d3d10core;
+    call.factory = (ULONG_PTR)factory;
+    call.adapter = (ULONG_PTR)adapter;
+    call.flags = flags;
+    call.feature_levels = (ULONG_PTR)feature_levels;
+    call.level_count = level_count;
+    call.device = (ULONG_PTR)device;
+
+    qemu_syscall(&call.super);
+
+    return call.super.iret;
+}
+
+#else
+
+extern HRESULT WINAPI DXGID3D10CreateDevice(HMODULE d3d10core, IDXGIFactory *factory, IDXGIAdapter *adapter,
+        unsigned int flags, const D3D_FEATURE_LEVEL *feature_levels, unsigned int level_count, void **device);
+void qemu_DXGID3D10CreateDevice(struct qemu_syscall *call)
+{
+    struct qemu_DXGID3D10CreateDevice *c = (struct qemu_DXGID3D10CreateDevice *)call;
+    WINE_FIXME("Unverified!\n");
+    c->super.iret = DXGID3D10CreateDevice(QEMU_G2H(c->d3d10core), QEMU_G2H(c->factory), QEMU_G2H(c->adapter),
+            c->flags, QEMU_G2H(c->feature_levels), c->level_count, QEMU_G2H(c->device));
+}
+
+#endif
+
+struct qemu_DXGID3D10RegisterLayers
+{
+    struct qemu_syscall super;
+    uint64_t layers;
+    uint64_t layer_count;
+};
+
+#ifdef QEMU_DLL_GUEST
+
+WINBASEAPI HRESULT WINAPI DXGID3D10RegisterLayers(const void *layers, UINT layer_count)
+{
+    struct qemu_DXGID3D10RegisterLayers call;
+    call.super.id = QEMU_SYSCALL_ID(CALL_DXGID3D10REGISTERLAYERS);
+    call.layers = (ULONG_PTR)layers;
+    call.layer_count = layer_count;
+
+    qemu_syscall(&call.super);
+
+    return call.super.iret;
+}
+
+#else
+
+extern HRESULT WINAPI DXGID3D10RegisterLayers(const void *layers, UINT layer_count);
+void qemu_DXGID3D10RegisterLayers(struct qemu_syscall *call)
+{
+    struct qemu_DXGID3D10RegisterLayers *c = (struct qemu_DXGID3D10RegisterLayers *)call;
+    WINE_FIXME("Unverified!\n");
+    c->super.iret = DXGID3D10RegisterLayers(QEMU_G2H(c->layers), c->layer_count);
+}
+
 #endif
 
 struct qemu_dll_init
@@ -64,6 +247,11 @@ const struct qemu_ops *qemu_ops;
 
 static const syscall_handler dll_functions[] =
 {
+    qemu_CreateDXGIFactory,
+    qemu_CreateDXGIFactory1,
+    qemu_CreateDXGIFactory2,
+    qemu_DXGID3D10CreateDevice,
+    qemu_DXGID3D10RegisterLayers,
     qemu_init_dll,
 };
 
