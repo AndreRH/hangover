f8cacc667bb31d80b669c13b1d49907efa622e26
Stefan Dösinger <stefan@codeweavers.com>
2018-06-08 16:23:14 +0200

    d3d11: CopySubresourceRegion1.

--- dlls/d3d11/device.c
+++ dlls/d3d11/device.c
@@ -6463,12 +6463,14 @@ struct qemu_d3d11_immediate_context_CopySubresourceRegion1
 {
     struct qemu_syscall super;
     uint64_t iface;
-    uint64_t dst_resource;
+    uint64_t dst_texture;
+    uint64_t dst_buffer;
     uint64_t dst_subresource_idx;
     uint64_t dst_x;
     uint64_t dst_y;
     uint64_t dst_z;
-    uint64_t src_resource;
+    uint64_t src_texture;
+    uint64_t src_buffer;
     uint64_t src_subresource_idx;
     uint64_t src_box;
     uint64_t flags;
@@ -6476,22 +6478,71 @@ struct qemu_d3d11_immediate_context_CopySubresourceRegion1
 
 #ifdef QEMU_DLL_GUEST
 
-static void STDMETHODCALLTYPE d3d11_immediate_context_CopySubresourceRegion1(ID3D11DeviceContext1 *iface, ID3D11Resource *dst_resource, UINT dst_subresource_idx, UINT dst_x, UINT dst_y, UINT dst_z, ID3D11Resource *src_resource, UINT src_subresource_idx, const D3D11_BOX *src_box, UINT flags)
+static void STDMETHODCALLTYPE d3d11_immediate_context_CopySubresourceRegion1(ID3D11DeviceContext1 *iface,
+        ID3D11Resource *dst_resource, UINT dst_subresource_idx, UINT dst_x, UINT dst_y, UINT dst_z,
+        ID3D11Resource *src_resource, UINT src_subresource_idx, const D3D11_BOX *src_box, UINT flags)
 {
     struct qemu_d3d11_immediate_context_CopySubresourceRegion1 call;
     struct qemu_d3d11_device_context *context = impl_from_ID3D11DeviceContext1(iface);
+    D3D11_RESOURCE_DIMENSION dim;
 
     call.super.id = QEMU_SYSCALL_ID(CALL_D3D11_IMMEDIATE_CONTEXT_COPYSUBRESOURCEREGION1);
     call.iface = (ULONG_PTR)context;
-    call.dst_resource = (ULONG_PTR)dst_resource;
     call.dst_subresource_idx = dst_subresource_idx;
     call.dst_x = dst_x;
     call.dst_y = dst_y;
     call.dst_z = dst_z;
-    call.src_resource = (ULONG_PTR)src_resource;
     call.src_subresource_idx = src_subresource_idx;
     call.src_box = (ULONG_PTR)src_box;
     call.flags = flags;
+
+    ID3D11Resource_GetType(dst_resource, &dim);
+    switch (dim)
+    {
+        case D3D11_RESOURCE_DIMENSION_TEXTURE1D:
+            call.dst_buffer = 0;
+            call.dst_texture = (ULONG_PTR)unsafe_impl_from_ID3D11Texture1D((ID3D11Texture1D *)dst_resource);
+            break;
+
+        case D3D11_RESOURCE_DIMENSION_TEXTURE2D:
+            call.dst_buffer = 0;
+            call.dst_texture = (ULONG_PTR)unsafe_impl_from_ID3D11Texture2D((ID3D11Texture2D *)dst_resource);
+            break;
+
+        case D3D11_RESOURCE_DIMENSION_TEXTURE3D:
+            call.dst_buffer = 0;
+            call.dst_texture = (ULONG_PTR)unsafe_impl_from_ID3D11Texture3D((ID3D11Texture3D *)dst_resource);
+            break;
+
+        case D3D11_RESOURCE_DIMENSION_BUFFER:
+            call.dst_buffer = (ULONG_PTR)unsafe_impl_from_ID3D11Buffer((ID3D11Buffer *)dst_resource);
+            call.dst_texture = 0;
+            break;
+    }
+
+    ID3D11Resource_GetType(src_resource, &dim);
+    switch (dim)
+    {
+        case D3D11_RESOURCE_DIMENSION_TEXTURE1D:
+            call.src_buffer = 0;
+            call.src_texture = (ULONG_PTR)unsafe_impl_from_ID3D11Texture1D((ID3D11Texture1D *)src_resource);
+            break;
+
+        case D3D11_RESOURCE_DIMENSION_TEXTURE2D:
+            call.src_buffer = 0;
+            call.src_texture = (ULONG_PTR)unsafe_impl_from_ID3D11Texture2D((ID3D11Texture2D *)src_resource);
+            break;
+
+        case D3D11_RESOURCE_DIMENSION_TEXTURE3D:
+            call.src_buffer = 0;
+            call.src_texture = (ULONG_PTR)unsafe_impl_from_ID3D11Texture3D((ID3D11Texture3D *)src_resource);
+            break;
+
+        case D3D11_RESOURCE_DIMENSION_BUFFER:
+            call.src_buffer = (ULONG_PTR)unsafe_impl_from_ID3D11Buffer((ID3D11Buffer *)src_resource);
+            call.src_texture = 0;
+            break;
+    }
 
     qemu_syscall(&call.super);
 }
@@ -6500,13 +6551,40 @@ static void STDMETHODCALLTYPE d3d11_immediate_context_CopySubresourceRegion1(ID3
 
 void qemu_d3d11_immediate_context_CopySubresourceRegion1(struct qemu_syscall *call)
 {
-    struct qemu_d3d11_immediate_context_CopySubresourceRegion1 *c = (struct qemu_d3d11_immediate_context_CopySubresourceRegion1 *)call;
+    struct qemu_d3d11_immediate_context_CopySubresourceRegion1 *c =
+            (struct qemu_d3d11_immediate_context_CopySubresourceRegion1 *)call;
     struct qemu_d3d11_device_context *context;
+    ID3D11Resource *src, *dst;
+    struct qemu_d3d11_texture *tex;
+    struct qemu_d3d11_buffer *buf;
 
-    WINE_FIXME("Unverified!\n");
+    WINE_TRACE("\n");
     context = QEMU_G2H(c->iface);
 
-    ID3D11DeviceContext1_CopySubresourceRegion1(context->host, QEMU_G2H(c->dst_resource), c->dst_subresource_idx, c->dst_x, c->dst_y, c->dst_z, QEMU_G2H(c->src_resource), c->src_subresource_idx, QEMU_G2H(c->src_box), c->flags);
+    if (c->src_buffer)
+    {
+        buf = QEMU_G2H(c->src_buffer);
+        src = (ID3D11Resource *)buf->host11;
+    }
+    else if (c->src_texture)
+    {
+        tex = QEMU_G2H(c->src_texture);
+        src = (ID3D11Resource *)tex->host11_1d;
+    }
+
+    if (c->dst_buffer)
+    {
+        buf = QEMU_G2H(c->dst_buffer);
+        dst = (ID3D11Resource *)buf->host11;
+    }
+    else if (c->dst_texture)
+    {
+        tex = QEMU_G2H(c->dst_texture);
+        dst = (ID3D11Resource *)tex->host11_1d;
+    }
+
+    ID3D11DeviceContext1_CopySubresourceRegion1(context->host, dst, c->dst_subresource_idx,
+            c->dst_x, c->dst_y, c->dst_z, src, c->src_subresource_idx, QEMU_G2H(c->src_box), c->flags);
 }
 
 #endif
