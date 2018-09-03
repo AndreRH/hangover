cfec8c20927e5f1b71bbc2a60894c9882cb182ea
Stefan Dösinger <stefan@codeweavers.com>
2018-09-03 20:59:40 +0200

    windowscodecs: Add empty wrapper.

---
+++ dlls/windowscodecs/qemu_windowscodecs.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 André Hentschel
+ * Copyright 2018 Stefan Dösinger for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef QEMU_WINDOWSCODECS_H
+#define QEMU_WINDOWSCODECS_H
+
+enum windowscodecs_calls
+{
+    CALL_INIT_DLL = 0,
+};
+
+#ifdef QEMU_DLL_GUEST
+
+#else
+
+extern const struct qemu_ops *qemu_ops;
+
+#endif
+
+#endif
