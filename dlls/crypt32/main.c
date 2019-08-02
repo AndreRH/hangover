4ab5daf204dafbeed425f91e92a24552ec950a47
Stefan Dösinger <stefan@codeweavers.com>
2019-08-02 17:59:02 +0200

    crypt32: Link to standard libs.

--- dlls/crypt32/main.c
+++ dlls/crypt32/main.c
@@ -818,7 +818,7 @@ struct qemu_dll_init
 
 #ifdef QEMU_DLL_GUEST
 
-BOOL WINAPI DllMainCRTStartup(HMODULE mod, DWORD reason, void *reserved)
+BOOL WINAPI DllMain(HMODULE mod, DWORD reason, void *reserved)
 {
     struct qemu_dll_init call;
 
